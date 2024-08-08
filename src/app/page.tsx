c91256fe6858b99d022ec8cfadaaba006153a644
Antonio Felix <afelixjdev@gmail.com>
2024-08-08 06:31:21 -0400

    refreshing repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,12 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-              <img src="/snake-1.png" className="w-full"/>
+                <img src="/snake-1.png" className="w-full" />
               </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a <span className="bg-green-600 px-2 text-white">Custom</span> Phone Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">Capture your favorite memories with your own, <span className="font-semibold">one-of-one</span> phone case. Case Cobra allws you to protect your memories, not just your phone.</p>
             </div>
           </div>
         </MaxWidthWrapper>
